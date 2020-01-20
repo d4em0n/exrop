ab3868bba5e61c7da26e3bcd8b7726ea6b28be3d
n0psledbyte <n0psledbyte@gmail.com>
2020-01-20 17:45:24 +0700

    update tests

--- tests.py
+++ tests.py
@@ -2,6 +2,9 @@ from Exrop import Exrop
 
 rop = Exrop("/bin/ls")
 rop.find_gadgets(cache=True)
-#chain = rop.set_regs({'rdi':0x41414141, 'rsi': 0x42424242, 'rdx':0x43434343, 'rax':0x44444444})
+print("write-regs gadget: rdi=0x41414141, rsi:0x42424242, rdx: 0x43434343, rax:0x44444444")
+chain = rop.set_regs({'rdi':0x41414141, 'rsi': 0x42424242, 'rdx':0x43434343, 'rax':0x44444444})
+chain.dump()
+print("write-what-where gadgets: [0x41414141]=0xdeadbeefff, [0x43434343]=0x110011")
 chain = rop.set_writes({0x41414141: 0xdeadbeefff, 0x43434343: 0x00110011})
 chain.dump()
