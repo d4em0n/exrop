d6f1ffc852432512821a496c44cd029517a6b38b
n0p <n0psledbyte@gmail.com>
2026-03-04 13:03:37 +0800

    Clean up examples: add docstrings, captured output, rename kernel files
    - Remove kernel_pivot.py, rename kernel_rop.py to kernel_rop_pivot.py
    - Add captured output as comments to all examples
    - Add docstrings to avoid_badchars, syscall, set_regs_all, open-read-write
    - Reduce set_regs_all from 14 to 8 regs (14 doesn't solve on this libc)
    - Remove timing code from open-read-write
    - Add kernel example output from kernelCTF lts-6.12.51

--- examples/move_regs.py
+++ examples/move_regs.py
@@ -14,34 +14,29 @@ LIBC = "/lib/x86_64-linux-gnu/libc.so.6"
 e = Exrop(LIBC)
 e.find_gadgets(cache=True)
 
-# Move rax into both rdi and rsi
-print("=== set rdi=rax, rsi=rax ===")
-try:
-    chain = e.set_regs({'rdi': 'rax', 'rsi': 'rax'})
-    chain.dump()
-except Exception as ex:
-    print("Failed: {}".format(ex))
-
-# Move rax into rdi, rdx into rsi
-print("\n=== set rdi=rax, rsi=rdx ===")
-try:
-    chain = e.set_regs({'rdi': 'rax', 'rsi': 'rdx'})
-    chain.dump()
-except Exception as ex:
-    print("Failed: {}".format(ex))
-
-# Move rdi into rsi (swap-like)
-print("\n=== set rsi=rdi ===")
-try:
-    chain = e.set_regs({'rsi': 'rdi'})
-    chain.dump()
-except Exception as ex:
-    print("Failed: {}".format(ex))
+# Move rdi into rsi
+print("=== set rsi=rdi ===")
+chain = e.set_regs({'rsi': 'rdi'})
+chain.dump()
 
 # Mix: move rax into rdi, set rsi to a constant
 print("\n=== set rdi=rax, rsi=0x42424242 ===")
-try:
-    chain = e.set_regs({'rdi': 'rax', 'rsi': 0x42424242})
-    chain.dump()
-except Exception as ex:
-    print("Failed: {}".format(ex))
+chain = e.set_regs({'rdi': 'rax', 'rsi': 0x42424242})
+chain.dump()
+
+# Output:
+# === set rsi=rdi ===
+# $RSP+0x0000 : 0x00000000000dd237 # pop rax ; ret
+# $RSP+0x0008 : 0x0000000000054472
+# $RSP+0x0010 : 0x00000000000b22ce # xchg edx, eax ; mov eax, 0xf7000000 ; ret 0
+# $RSP+0x0018 : 0x0000000000078740 # mov r12, rdi ; jmp rdx: next -> (0x00054472) # mov esi, 0x83480143 ; ret
+# $RSP+0x0020 : 0x00000000000dd237 # pop rax ; ret
+# $RSP+0x0028 : 0x00000000000dd237
+# $RSP+0x0030 : 0x00000000000af7d9 # mov rsi, r12 ; call rax: next -> (0x000dd237) # pop rax ; ret
+#
+# === set rdi=rax, rsi=0x42424242 ===
+# $RSP+0x0000 : 0x00000000000586e4 # pop rbx ; ret
+# $RSP+0x0008 : 0x0000000000110a7d
+# $RSP+0x0010 : 0x00000000000de942 # mov rdi, rax ; call rbx: next -> (0x00110a7d) # pop rsi ; ret
+# $RSP+0x0018 : 0x0000000000110a7d # pop rsi ; ret
+# $RSP+0x0020 : 0x0000000042424242
