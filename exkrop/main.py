871ded05eb246a4c0421aa672b5c38ff8868683b
n0p <n0psledbyte@gmail.com>
2026-03-05 12:53:18 +0800

    Add rbp to pivot register list

--- exkrop/main.py
+++ exkrop/main.py
@@ -11,7 +11,7 @@ from Exrop import Exrop
 
 PAGE_SIZE = 10
 PIVOT_REGISTERS = [
-    'rdi', 'rsi', 'rdx', 'rcx', 'rbx',
+    'rdi', 'rsi', 'rdx', 'rcx', 'rbx', 'rbp',
     'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
 ]
 
